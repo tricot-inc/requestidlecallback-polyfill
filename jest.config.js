c207eb7e67b501bc84ca4fe3313d91c2d7150f06
Ria <ria000130@gmail.com>
2021-08-06 18:20:24 +0900

    test: requestIdleCallbackのtest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  testMatch: null,
-  testRegex: '(/__tests__/.*)\\.test\\.(ts)$',
-  collectCoverageFrom: ['**/*.ts'],
-  coveragePathIgnorePatterns: ['__mocks__', 'node_modules', 'types'],
-};
